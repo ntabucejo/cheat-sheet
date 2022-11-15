c3d9d6013a111da261143f09dd85c738809f35de
ntabucejo <ntabucejo@gmail.com>
2022-11-16 00:35:05 +0800

    chore(typescript): add tupple.ts

---
+++ typescript/examples/tupple.ts
@@ -0,0 +1,17 @@
+// Typescript
+
+// ----------------------------------------------------------------
+
+// Tupple
+// ? A tuple is a typed array with a pre-defined length and types for each index
+
+type ColorSaturation = [string, number];
+
+let colorSaturation: ColorSaturation = ["blue", 100];
+let emotionRate: [string, number] = ["happy", 60];
+
+// ----------------------------------------------------------------
+
+// By: Nikko Abucejo
+
+export {};
