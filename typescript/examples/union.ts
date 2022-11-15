982333ecdd7cea3c9ba3e10c59cbaeca9ffeefa5
ntabucejo <ntabucejo@gmail.com>
2022-11-16 00:35:14 +0800

    chore(typescript): add union.ts

---
+++ typescript/examples/union.ts
@@ -0,0 +1,17 @@
+// Typescript
+
+// ----------------------------------------------------------------
+
+// Union
+// ? A union type describes a value that can be one of several types
+
+type RulerWidth = number | string;
+
+let rulerWidth: RulerWidth = 10;
+let rulerHeight: number | string = "10";
+
+// ----------------------------------------------------------------
+
+// By: Nikko Abucejo
+
+export {};
